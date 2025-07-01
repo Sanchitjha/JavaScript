71e4a4f5bd582449b9cbd213e64848245452ad81
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-02 02:59:33 +0530

    datatype summary

---
+++ Java Solutions/datatypes.sumaries.js
@@ -0,0 +1,36 @@
+const score = 100
+const scoreValue = 100.3 
+
+
+const isLoggedIn = false
+const outsideTemp =null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// Non primitive
+//Array , Objects , Functions
+
+const heros = ["shaktiman", "spiderman", "Batman"]
+
+let myObj = {
+    name: "Sanchit",
+    age: 22, 
+}
+
+const myFunction = function () {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof heros);
+
+
+
+
+
+
