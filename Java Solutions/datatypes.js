52d4f9dfc96048f681968e7f92c47ff822000a86
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-01 18:27:21 +0530

    Datatypes

---
+++ Java Solutions/datatypes.js
@@ -0,0 +1,6 @@
+let Nmae ="Sanchit Jha"
+let age = 22
+let isloggedIn = false
+let state = null
+
+console.log(typeof "Sanchit Jha");
---
+++ Java Solutions/variable.js
@@ -0,0 +1,12 @@
+const accountID = 144553
+let accountEmail ="sanchitjha8888@gmail.com"
+var accountPassword="12345"
+accountcity ="Bihar"
+
+accountEmail="jhasanchit@gmail.com"
+accountPassword ="12453464"
+accountcity ="Surat"
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountcity]);
+
