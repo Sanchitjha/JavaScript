43d83f8dc6c7ff9789a039d80c3b83f44be755f5
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-02 02:00:54 +0530

    operations

--- Java Solutions/conversionoperation.js
+++ Java Solutions/conversionoperation.js
@@ -14,8 +14,41 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//operations
+ 
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(4-2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
 
 
 
