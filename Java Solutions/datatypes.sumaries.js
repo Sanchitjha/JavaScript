cc87722a068758c49bde032626a1015532bdc99b
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-03 01:07:09 +0530

    MemoryAllocation

--- Java Solutions/datatypes.sumaries.js
+++ Java Solutions/datatypes.sumaries.js
@@ -29,6 +29,34 @@ const myFunction = function () {
 
 console.log(typeof heros);
 
+//Memory
+
+//Stack(Primitive) ,Heap(Non-Premitive)
+
+let myCollegename = "Bhagwan Mahavir"
+
+let anothername = myCollegename
+anothername = "DRB"
+
+console.log(myCollegename);
+console.log(anothername);
+
+let user1 = {
+        email: "sanchitjha8888@gmail.com",
+        upi: "sanchit@123"
+    }
+
+let user2 = user1
+
+user2.email = "jhasanchit8888@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
+
+
+
+
+
 
 
 
