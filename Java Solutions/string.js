f4dac4cfbc1821d7d9774febbc085c92b113dfaa
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-04 15:15:45 +0530

    String

---
+++ Java Solutions/string.js
@@ -0,0 +1,15 @@
+const name = "Sanchit"
+const repoCount = 4
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("jhaSanchit")
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.lastIndexOf ('t'));
+
+
