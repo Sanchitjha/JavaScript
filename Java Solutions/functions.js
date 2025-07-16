24de2d0c6f20df55dd440c9255d8a8a98ceafcff
Sanchitjha <sanchitjha8888@gmail.com>
2025-07-16 23:23:01 +0530

    functions

--- Java Solutions/functions.js
+++ Java Solutions/functions.js
@@ -34,5 +34,36 @@ function loginUserMessage(username){
       return `${username} just logged in`
 }
 //console.log (loginUserMessage("Sanchit"))
-console.log (loginUserMessage())
+// console.log (loginUserMessage())
 
+function calculateCartprice(val1,val2,...num1){
+    return num1
+}
+//console.log(calculateCartprice(200,300,400,500))
+
+const user = {
+    username: "Sanchit",
+    Price: 499
+}
+const user2 = {
+    username: "Manav",
+    Price: 599
+}
+
+function handleObject (anyobject) {
+    console.log(`Username is ${anyobject.username} and The Price is ${anyobject.Price}`);
+}
+
+// handleObject(user)
+// handleObject(user2)
+handleObject({
+    username: "Omkar",
+    Price: 399
+})
+
+const NewArray = [200,300,400,500]
+ function returnsecondValue(Array){
+      return Array[1]
+ }
+//  console.log(returnsecondValue(NewArray));
+// console.log(returnsecondValue([200,300,400,500])); 
